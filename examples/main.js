1a9d5a093fcf39af8f5ce91d7706387c74a39218
yadongqu <yadong.chu@gmail.com>
2017-05-06 14:58:46 +0200

    add buttons and blockquote

---
+++ examples/main.js
@@ -0,0 +1,30 @@
+// var app = document.getElementById("app");
+
+// const grid = document.createElement("div");
+// grid.className = "container";
+// grid.innerHTML = "<h3>Grid System</h3>";
+// grid.style.marginBottom = "1em";
+// app.appendChild(grid)
+
+// for (var i = 1; i < 12; i++){
+//     let container = document.createElement("div");
+//     container.className = "container";
+//     container.classList.add("center");
+//     let inner1 = document.createElement("div");
+//     inner1.className = "col-" + i;
+//     inner1.classList.add("bg-aqua");
+//     inner1.innerHTML = i;
+//     let inner2 = document.createElement("div");
+//     inner2.className = "col-" + (12 - i);
+//     inner2.innerHTML = 12 - i;
+//     container.appendChild(inner1);
+//     container.appendChild(inner2);
+//     app.appendChild(container);
+// }
+
+
+// const buttons = document.createElement("div");
+// buttons.className = "container";
+// buttons.innerHTML = "<h3>Buttons</h3>";
+// buttons.style.marginBottom = "1em";
+// app.appendChild(buttons)
